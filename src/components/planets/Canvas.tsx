17ceaeb19520225d151c3a245b306579ef984153
ludivineConstanti <ludivine.constanti@outlook.fr>
2024-02-18 16:50:04 +0100

    fix: added a resize event to change the layout of the 3D scene on mobile

--- src/components/planets/Canvas.tsx
+++ src/components/planets/Canvas.tsx
@@ -6,16 +6,24 @@ import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import Composer from "./Composer";
 import PlanetsAndStars from "./PlanetsAndStars";
 import Camera from "./Camera";
+import { useResizeObserver } from "@/hooks";
 
 const CanvasComponent = () => {
   const [scrollPositionY, setScrollPositionY] = useState(0);
+  const [width, setWidth] = useState(0);
 
   useScrollPosition(({ currPos }) => {
     setScrollPositionY(currPos.y);
   });
 
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  useResizeObserver(ref.current, ({ clientWidth }) => {
+    setWidth(window ? window.innerWidth : clientWidth);
+  });
+
   return (
-    <div className="fixed top-0 h-full w-full">
+    <div className="fixed top-0 h-full w-full" ref={ref}>
       <Canvas
         gl={{
           antialias: true,
@@ -27,7 +35,7 @@ const CanvasComponent = () => {
       >
         <Composer scrollPositionY={scrollPositionY} />
         <Camera />
-        <PlanetsAndStars scrollPositionY={scrollPositionY} />
+        <PlanetsAndStars scrollPositionY={scrollPositionY} width={width} />
       </Canvas>
     </div>
   );
