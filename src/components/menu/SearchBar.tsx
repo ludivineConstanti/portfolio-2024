ebf244aa193895209e04b38a50753dbbfb717a0f
ludivineConstanti <ludivine.constanti@outlook.fr>
2024-02-25 20:37:18 +0100

    fix: added a max width for the search bar on tablet

--- src/components/menu/SearchBar.tsx
+++ src/components/menu/SearchBar.tsx
@@ -30,7 +30,7 @@ const SearchBar = ({
       }}
       classNames={{
         container: () =>
-          "outline-2 outline-white text-body min-h-fit text-blue-950 border-solid min-w-44 max-w-[calc(100vw-10rem)] xl:max-w-[40rem] border-2 border-blue-950 min-w-30 px-2 bg-white rounded-2xl pointer-events-auto",
+          "outline-2 outline-white text-body min-h-fit text-blue-950 border-solid min-w-44 sm:max-w-52 max-w-[calc(100vw-10rem)] xl:max-w-[40rem] border-2 border-blue-950 min-w-30 px-2 bg-white rounded-2xl pointer-events-auto",
         dropdownIndicator: () => "[&>svg>path]:fill-blue-800 cursor-pointer",
         control: () => "min-h-fit",
         placeholder: () => "text-gray-500",
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -28,6 +28,8 @@ const menuLinksKeys = internalLinksKeys.filter(
   (e) => internalLinks[e].showInMenu,
 );
 
+const classNameMenuGap = "gap-8 sm:gap-4 xl:gap-8";
+
 const Menu = ({
   colorPrimary,
   colorSecondary,
@@ -76,16 +78,27 @@ const Menu = ({
         setMenuIsOpen={setMenuIsOpen}
         color={colorPrimary}
       />
-      <div className="flex w-full justify-center gap-8 sm:absolute sm:top-6 sm:items-start xl:top-7">
+      <div
+        className={clsx(
+          classNameMenuGap,
+          "z-10 flex w-full justify-center sm:absolute sm:top-6 sm:mx-4 sm:items-start xl:top-7",
+        )}
+      >
         <div
           className={clsx(
             { hidden: !menuIsOpen },
             colorPrimary,
-            "flex w-full flex-col items-center justify-center gap-8 p-16 sm:flex sm:w-fit sm:flex-row sm:bg-[rgba(255,255,255,0)] sm:p-0",
+            classNameMenuGap,
+            "flex w-full flex-col items-center justify-center p-16 sm:flex sm:w-fit sm:flex-row sm:bg-[rgba(255,255,255,0)] sm:p-0",
           )}
         >
           <nav>
-            <ul className="flex flex-col items-center justify-center gap-8 sm:flex-row">
+            <ul
+              className={clsx(
+                classNameMenuGap,
+                "flex flex-col items-center justify-center sm:flex-row",
+              )}
+            >
               {menuLinksKeys
                 .filter((internalLinkKey) => internalLinkKey !== pageId)
                 .map((internalLinkKey) => (
