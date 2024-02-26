7ecf4d286070df7a56fd5b7211f5657f73d67bf8
ludivineConstanti <ludivine.constanti@outlook.fr>
2024-02-26 13:16:37 +0100

    fix: height search bar

--- src/components/menu/SearchBar.tsx
+++ src/components/menu/SearchBar.tsx
@@ -3,11 +3,19 @@
 import dynamic from "next/dynamic";
 import React from "react";
 import { SearchBarComponentProps } from "@/models";
+import type { CSSObjectWithLabel } from "react-select";
 import { useAppDispatch } from "@/store";
 import { setSelectedSkillsFilter } from "@/store/slices/system";
 import { useAppSelector } from "@/store";
 
 const Select = dynamic(() => import("react-select"));
+
+const styles = {
+  control: (baseStyles: CSSObjectWithLabel) => ({
+    ...baseStyles,
+    minHeight: 0,
+  }),
+};
 
 const SearchBar = ({
   skillsFilter,
@@ -28,11 +36,12 @@ const SearchBar = ({
       onChange={(v) => {
         dispatch(setSelectedSkillsFilter(v));
       }}
+      styles={styles}
       classNames={{
         container: () =>
           "outline-2 outline-white text-body min-h-fit text-blue-950 border-solid min-w-44 sm:max-w-52 max-w-[calc(100vw-10rem)] xl:max-w-[40rem] border-2 border-blue-950 min-w-30 px-2 bg-white rounded-2xl pointer-events-auto",
         dropdownIndicator: () => "[&>svg>path]:fill-blue-800 cursor-pointer",
-        control: () => "min-h-fit",
+        control: () => "min-h-0",
         placeholder: () => "text-gray-500",
         // dropdown
         menuList: () => "-bottom-2 -left-2 rounded-lg py-2 bg-white",
